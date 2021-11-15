267394b94abf5472029468cc07052da16efb0f8d
OlaAbdQad <olanrewajuabdulqadir@gmail.com>
2021-11-15 13:18:03 +0100

    new record

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace old
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
+        }
+    }
+}
